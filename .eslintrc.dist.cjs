8fba01ea6c0db9f11c1bf2f2c9c670d14bad7241
Sebastian Wiesner <sebastian@swsnr.de>
2023-11-15 15:06:14 +0100

    Fix formatting

--- .eslintrc.dist.cjs
+++ .eslintrc.dist.cjs
@@ -3,18 +3,18 @@ module.exports = {
   extends: [],
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
     "lines-between-class-members": ["error", "always"],
     "padding-line-between-statements": [
       "error",
-      { "blankLine": "always", "prev": "*", "next": "class" },
-      { "blankLine": "always", "prev": "*", "next": "function" },
-      { "blankLine": "always", "prev": "*", "next": "return" },
-      { "blankLine": "always", "prev": "*", "next": "export" },
-      { "blankLine": "always", "prev": "*", "next": "multiline-const" },
-    ]
+      { blankLine: "always", prev: "*", next: "class" },
+      { blankLine: "always", prev: "*", next: "function" },
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "always", prev: "*", next: "export" },
+      { blankLine: "always", prev: "*", next: "multiline-const" },
+    ],
   },
-  ignorePatterns: []
+  ignorePatterns: [],
 };
